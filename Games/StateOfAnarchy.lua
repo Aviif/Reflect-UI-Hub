f0391bc03f89f98fb9b853b90558033599d2c81b
Aviif <72271503+Aviif@users.noreply.github.com>
2022-06-22 17:02:50 +0200

    updated

--- Games/StateOfAnarchy.lua
+++ Games/StateOfAnarchy.lua
@@ -1,10 +1,11 @@
 --// Game ID = 2000062521
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local colors = {
-    SchemeColor = Color3.fromRGB(0,255,255),
-    Background = Color3.fromRGB(0, 0, 0),
-    Header = Color3.fromRGB(0, 0, 0),
+    SchemeColor = Color3.fromRGB(226, 0, 116),
+    Background = Color3.fromRGB(69, 69, 69),
+    Header = Color3.fromRGB(69, 69, 69),
     TextColor = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(20, 20, 20)
+    ElementColor = Color3.fromRGB(69, 69, 69)
 }
 local Window = Library.CreateLib("Reflect Hub - State of Anarchy", "Color")
+local playerSettings = Window:NewTab("Player Settings")
