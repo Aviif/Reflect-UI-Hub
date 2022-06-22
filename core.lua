fc5e5d42b11fe623c5eca7ab51f675685e26f4d2
Aviif <72271503+Aviif@users.noreply.github.com>
2022-06-22 23:53:13 +0200

    update

--- core.lua
+++ core.lua
@@ -2,7 +2,6 @@
 
 local PlaceIDs = {255236425, 2000062521, 5265353117, 185655149, 142823291, 1458767429, 2753915549, 1537690962, 4623386862, 920587237, 370731277}
 
-
 for i, v in pairs(PlaceIDs) do
     if v == game.PlaceId then
         loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/" .. game.PlaceId .. ".lua"))() 
