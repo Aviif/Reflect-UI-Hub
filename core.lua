b89b4c2b94ad930d484b515ff95289e8e72f3511
Aviif <72271503+Aviif@users.noreply.github.com>
2022-06-22 23:44:30 +0200

    updated

--- core.lua
+++ core.lua
@@ -1,10 +1,10 @@
 --// This script loads the game specific UI for Reflect.
 
-local gameIDs = {255236425, 2000062521, 5265353117, 185655149, 142823291, 1458767429, 2753915549, 1537690962, 4623386862, 920587237, 370731277}
+local PlaceIDs = {255236425, 2000062521, 5265353117, 185655149, 142823291, 1458767429, 2753915549, 1537690962, 4623386862, 920587237, 370731277}
 
 
-if game.GameId == table.find(gameIDs) then
-    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/" .. game.GameId .. ".lua"))()
+if game.PlaceId == table.find(PlaceIDs) then
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/" .. game.PlaceId .. ".lua"))()
 end 
 --[[
 if game.PlaceId == 255236425 then
