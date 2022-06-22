eeb383762e06c540733c87cd0955315d2e2fbd59
Aviif <72271503+Aviif@users.noreply.github.com>
2022-06-22 16:41:00 +0200

    Update core.lua

---
+++ core.lua
@@ -0,0 +1,17 @@
+--// This script loads the game specific UI for Reflect.
+
+if game.PlaceId == 255236425 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/TTT.lua'))()
+end
+
+if game.PlaceId == 2000062521 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/StateOfAnarchy.lua'))()
+end
+
+if game.PlaceId == 185655149 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/Bloxburg.lua'))()
+end
+
+if game.PlaceId == 142823291 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/MM2.lua'))()
+end
