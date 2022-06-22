be344d47328f4c0cfc589e04d4e101ede9d8b43e
Aviif <72271503+Aviif@users.noreply.github.com>
2022-06-22 16:57:46 +0200

    updated

--- core.lua
+++ core.lua
@@ -1,21 +1,21 @@
 --// This script loads the game specific UI for Reflect.
 
 if game.PlaceId == 255236425 then
-    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/TTT.lua'))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/TTT.lua"))()
 end
 
 if game.PlaceId == 2000062521 then
-    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/StateOfAnarchy.lua'))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/StateOfAnarchy.lua"))()
 end
 
 if game.PlaceId == 185655149 then
-    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/Bloxburg.lua'))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/Bloxburg.lua"))()
 end
 
 if game.PlaceId == 142823291 then
-    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/MM2.lua'))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/MM2.lua"))()
 end
 
 if game.PlaceId == 5265353117 then
-    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/Betterscripting.lua'))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Games/BetterScripting.lua"))()
 end
