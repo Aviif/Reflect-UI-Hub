19d48c712f97d193b69bc8c7142632f8cb581727
Aviif <72271503+Aviif@users.noreply.github.com>
2022-06-22 16:50:44 +0200

    updated

--- Games/TTT.lua
+++ Games/TTT.lua
@@ -1,3 +1,10 @@
 --// Game ID = 255236425
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local colors = {
+    SchemeColor = Color3.fromRGB(0,255,255),
+    Background = Color3.fromRGB(0, 0, 0),
+    Header = Color3.fromRGB(0, 0, 0),
+    TextColor = Color3.fromRGB(255,255,255),
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
 local Window = Library.CreateLib("Reflect Hub - Traitor Town", "Color")
