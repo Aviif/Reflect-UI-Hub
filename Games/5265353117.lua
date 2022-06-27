6f7fb3d2030587bd6555188487c8f594c6122120
Aviif <72271503+Aviif@users.noreply.github.com>
2022-06-27 08:25:13 +0200

    update

--- Games/5265353117.lua
+++ Games/5265353117.lua
@@ -5,17 +5,21 @@ local colors = {
     Background = Color3.fromRGB(130, 130, 130),
     Header = Color3.fromRGB(69, 69, 69),
     TextColor = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(110, 110, 110)
+    ElementColor = Color3.fromRGB(69, 69, 69)
 }
 local Window = Library.CreateLib("Reflect Hub - " .. gameName, colors)
 
 local profileSettings = Window:NewTab("Profile information")
 local profileSection = profileSettings:NewSection("Here you can see infos about yourself")
 
+
 local playerSettings = Window:NewTab("Player Settings")
 local playerSection = playerSettings:NewSection("Player Options")
 playerSection:NewSlider("Walkspeeed", "Changes your walkspeed from a value between 16 to 500.", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+playerSection:NewSlider("Jump Power", "Changes your Jump Power from a value between 50 to 500.", 500, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 end)
 
 local scriptGameWindow = Window:NewTab(gameName)
@@ -32,15 +36,28 @@ end)
 scriptSection:NewButton("IY", "Loads Unnamed Infinite Yield", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Scripts/IY.lua"))()
 end)
+scriptSection:NewButton("Backdoor Finder", "Loads Backdoor.exe V4", function()
+    loadstring(game:HttpGet("https://the-shed.xyz/roblox/scripts/Backdoor", true))()
+end)
+
 scriptSection:NewButton("Remote Spy", "Loads Mister Spy V2", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Aviif/Reflect-UI-Hub/main/Scripts/RSpy.lua"))()
 end)
+scriptSection:NewButton("Sheds Bypass", "Loads Sheds Chat Bypasser V3", function()
+    loadstring(game:HttpGet("https://the-shed.xyz/roblox/scripts/ChatBypass", true))()
+end)
+
 
 local gameSettings = Window:NewTab("Game Settings")
 local gameSection = gameSettings:NewSection("Here you can view and change game settings")
 
+
 local colorPicker = Window:NewTab("Color Picker")
 local colorSection = colorPicker:NewSection("Here you can change the UI color")
+colorSection:NewColorPicker("RGB Color Picker", "Here you can change the colors of the UI", Color3.fromRGB(0,0,0), function(color)
+    print(color)
+   
+end)
 
 local credits = Window:NewTab("Credits")
-local creditsSection = credits:NewSection("This for the different credits")
+local creditsSection = credits:NewSection("This is for the different credits")
